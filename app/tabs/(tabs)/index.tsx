31ab7b96b137b1c89c3a082993932bc36c28fc92
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-13 08:03:15 +0300

    add the head chart old

--- app/tabs/(tabs)/index.tsx
+++ app/tabs/(tabs)/index.tsx
@@ -190,22 +190,6 @@ const DailyTab = () => {
     </Box>
   );
 
-  function randomNumber() {
-    return Math.floor(Math.random() * 26) + 125;
-  }
-  function generateRandomColor(): string {
-    // Generating a random number between 0 and 0xFFFFFF
-    const randomColor = Math.floor(Math.random() * 0xffffff);
-    // Converting the number to a hexadecimal string and padding with zeros
-    return `#${randomColor.toString(16).padStart(6, "0")}`;
-  }
-  const DATA = (numberPoints = 5) =>
-    Array.from({ length: numberPoints }, (_, index) => ({
-      value: randomNumber(),
-      color: generateRandomColor(),
-      label: `Label ${index + 1}`,
-    }));
-
   const ListHeader = () => (
     <Box className="pb-4 pt-8 px-4">
       <Text className="text-xl font-bold mb-4">Today's Summary</Text>
